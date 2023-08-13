0074dc185553e71f7904309088426d26de072a0e
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-08-13 20:37:07 +0000

    added in ability to show all sidebar content when in landscape. also disabled scrolling in the main content when the sidebar is open

--- valdezeyecenter/src/components/Sidebar.tsx
+++ valdezeyecenter/src/components/Sidebar.tsx
@@ -8,39 +8,50 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, setIsOpen }) => {
+  useEffect(() => {
+    const body = document.querySelector('body');
+    if (body) {
+      body.style.overflow = isOpen ? 'hidden' : 'auto';
+    }
+    return () => {
+      if (body) {
+        body.style.overflow = 'auto';
+      }
+    };
+  }, [isOpen]);
 
   const handleClose = () => {
     setIsOpen(false);
   };
 
   return (
-      <nav
-        className={`fixed right-0 top-0 bg-blue-400 border-4 border-r-0 border-double border-white z-20 w-screen h-screen transition-transform duration-500 transform ${isOpen ? 'translate-x-0 xl:hidden' : 'translate-x-full'
-          }`}
-      >
-        <div className="flex flex-col h-full m-6">
-          <button className="self-end text-white hover:bg-blue-800 py-1 px-4 rounded-lg" onClick={handleClose}>
-            Close
-          </button>
-          <div className="mt-6">
-            {navItems.map((item, index) => (
-              <React.Fragment key={index}>
-                <Link
-                  href={item.href}
-                  passHref
-                  onClick={handleClose}
-                  className="text-2xl block my-2 p-2 text-white hover:bg-blue-800 w-full rounded-lg"
-                >
-                  {item.text}
-                </Link>
-                {index < navItems.length - 1 && (
-                  <hr className="border-white h-1 my-2" />
-                )}
-              </React.Fragment>
-            ))}
-          </div>
+    <nav
+      className={`fixed right-0 top-0 bg-blue-400 border-4 border-r-0 border-double border-white z-20 w-screen h-screen transition-transform duration-500 transform ${isOpen ? 'translate-x-0 xl:hidden' : 'translate-x-full'
+        }`}
+    >
+      <div className="flex flex-col h-full m-6 overflow-y-scroll pb-16">
+        <button className="fixed self-end text-white hover:bg-blue-800 py-1 px-4 rounded-lg" onClick={handleClose}>
+          Close
+        </button>
+        <div className="mt-6 ">
+          {navItems.map((item, index) => (
+            <React.Fragment key={index}>
+              <Link
+                href={item.href}
+                passHref
+                onClick={handleClose}
+                className="text-2xl block my-2 p-2 text-white hover:bg-blue-800 w-full rounded-lg"
+              >
+                {item.text}
+              </Link>
+              {index < navItems.length - 1 && (
+                <hr className="border-white h-1 my-2" />
+              )}
+            </React.Fragment>
+          ))}
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 };
 
