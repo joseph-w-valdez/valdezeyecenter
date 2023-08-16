fdef28545e1cc0ff10f1c0bdbfd0f61cfb6424f7
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2023-08-16 14:20:43 +0000

    added in health conditions data file

--- valdezeyecenter/src/components/Footer.tsx
+++ valdezeyecenter/src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import Image from 'next/image'
 import logoFull from '../assets/images/logo-full.png'
 
---
+++ valdezeyecenter/src/data/healthConditions.ts
@@ -0,0 +1,147 @@
+export const healthConditions = [
+  {
+    name: "Amblyopia (Lazy Eye)",
+    description: "Amblyopia, commonly referred to as lazy eye, is a condition where one eye has weaker vision than the other due to inadequate visual stimulation during childhood. This can occur due to factors such as crossed eyes or significant differences in refractive errors between the two eyes. If left untreated, amblyopia can lead to permanent vision impairment in the weaker eye. Early intervention, often involving corrective eyewear and visual therapy, can help stimulate proper visual development and improve the weaker eye's function.",
+    url: "https://www.aao.org/eye-health/diseases/what-is-amblyopia"
+  },
+  {
+    name: "Astigmatism",
+    description: "Astigmatism is a common refractive error that results from an uneven curvature of the cornea or lens. This causes light to focus unevenly on the retina, leading to blurred or distorted vision. Astigmatism can occur alongside nearsightedness or farsightedness and is corrected with eyeglasses, contact lenses, or refractive surgery.",
+    url: "https://www.aao.org/eye-health/diseases/what-is-astigmatism"
+  },
+  {
+    name: "Blepharitis",
+    description: "Blepharitis is an inflammation of the eyelids, often caused by bacteria or skin conditions like seborrheic dermatitis. It leads to redness, itching, and crusting along the eyelid margins. While it's a chronic condition, proper eyelid hygiene and warm compresses can help manage symptoms. In some cases, medical treatments may be needed.",
+    url: "https://www.aao.org/eye-health/diseases/blepharitis"
+  },
+  {
+    name: "Cataracts",
+    description: "Cataracts involve the clouding of the eye's natural lens, which is normally clear and allows light to enter the eye. This clouding causes blurred vision, decreased visual clarity, and increased sensitivity to glare. Cataracts are a common age-related condition, but they can also develop as a result of factors such as injury, certain medications, or medical conditions. Cataract surgery, where the cloudy lens is replaced with an artificial lens, is a highly effective treatment option that can restore clear vision.",
+    url: "https://www.aao.org/eye-health/diseases/what-are-cataracts"
+  },
+  {
+    name: "Color Blindness",
+    description: "Color blindness, also known as color vision deficiency, is a genetic condition that affects an individual's ability to perceive certain colors accurately. This condition is more common in males due to the way the genes responsible for color vision are inherited. Most cases of color blindness result from the absence or dysfunction of specific color-sensing pigments in the retina. While there is no cure for color blindness, individuals with this condition can learn to adapt to their visual environment using strategies like color-coding and pattern recognition.",
+    url: "https://nei.nih.gov/health/color_blindness/facts_about"
+  },
+  {
+    name: "Conjunctivitis (Pink Eye)",
+    description: "Conjunctivitis, commonly known as pink eye, is an inflammation of the conjunctiva, the thin membrane covering the white part of the eye and the inner surface of the eyelids. It can be caused by viruses, bacteria, allergies, or irritants. Symptoms include redness, itching, tearing, and discharge. Treatment varies based on the underlying cause.",
+    url: "https://www.aao.org/eye-health/diseases/conjunctivitis-pink-eye"
+  },
+  {
+    name: "Corneal Ulcer",
+    description: "A corneal ulcer is an open sore on the cornea, typically caused by infection or injury. Symptoms include eye pain, redness, tearing, and blurred vision. Corneal ulcers can be serious and lead to vision loss if not treated promptly. Treatment may involve antibiotic or antifungal medications, as well as protective measures.",
+    url: "https://www.aao.org/eye-health/diseases/corneal-ulcer"
+  },
+  {
+    name: "Diabetic Retinopathy",
+    description: "Diabetic retinopathy is a complication of diabetes that impacts the blood vessels in the retina, the light-sensitive tissue at the back of the eye. Uncontrolled blood sugar levels can cause damage to these blood vessels, leading to various retinal abnormalities. Over time, diabetic retinopathy can result in vision problems and even blindness if not properly managed. Regular eye screenings are essential for individuals with diabetes to detect and treat this condition in its early stages.",
+    url: "https://www.diabetes.org/diabetic-retinopathy"
+  },
+  {
+    name: "Dry Eye Syndrome",
+    description: "Dry eye syndrome is a common condition characterized by insufficient tear production or poor tear quality, resulting in discomfort, irritation, and potential damage to the surface of the eye. Environmental factors, age, and certain medical conditions can contribute to the development of dry eye. Management strategies include artificial tears, lifestyle adjustments, and, in some cases, medical treatments to improve tear production and alleviate symptoms.",
+    url: "https://www.aao.org/eye-health/diseases/what-is-dry-eye"
+  },
+  {
+    name: "Glaucoma",
+    description: "Glaucoma refers to a group of eye conditions characterized by damage to the optic nerve, often as a result of elevated intraocular pressure (pressure within the eye). This damage leads to gradual vision loss, initially affecting peripheral vision and potentially progressing to complete blindness if left untreated. The condition develops when the drainage system within the eye becomes compromised, leading to a buildup of fluid and pressure.",
+    url: "https://www.glaucoma.org/glaucoma/"
+  },
+  {
+    name: "Hyperopia (Farsightedness)",
+    description: "Hyperopia, also known as farsightedness, is a refractive error where distant objects can be seen more clearly than close objects. It occurs when the eyeball is too short or the cornea is too flat. Hyperopia can lead to eyestrain and blurry near vision. Eyeglasses, contact lenses, or refractive surgery can correct hyperopia.",
+    url: "https://www.aao.org/eye-health/diseases/hyperopia-farsightedness"
+  },
+  {
+    name: "Keratoconus",
+    description: "Keratoconus is an eye disorder characterized by the thinning and conical deformation of the cornea, the transparent front surface of the eye. This irregular corneal shape results in distorted and blurred vision. The condition often begins during adolescence or early adulthood and can progress over time. Corrective eyewear, such as specially designed contact lenses, can help improve vision for individuals with keratoconus. In more severe cases, surgical interventions like corneal cross-linking or corneal transplantation may be considered.",
+    url: "https://www.aao.org/eye-health/diseases/what-is-keratoconus"
+  },
+  {
+    name: "Macular Degeneration",
+    description: "Macular degeneration is a progressive eye disease that primarily affects the central portion of the retina, known as the macula. This condition leads to a gradual loss of central vision, making it difficult to see fine details and perform activities that require sharp eyesight. Macular degeneration is a common cause of vision impairment, especially among older adults. The degeneration occurs due to the breakdown of light-sensitive cells in the macula, which are crucial for clear and detailed vision.",
+    url: "https://www.macular.org/what-macular-degeneration"
+  },
+  {
+    name: "Macular Hole",
+    description: "A macular hole is a small break or tear in the macula, the central portion of the retina responsible for detailed central vision. It can cause distortion, blurriness, and central vision loss. Macular holes are often related to age or eye trauma. Surgical procedures, such as a vitrectomy, can be performed to repair the hole and restore vision.",
+    url: "https://www.aao.org/eye-health/diseases/macular-hole"
+  },
+  {
+    name: "Myopia (Nearsightedness)",
+    description: "Myopia, also known as nearsightedness, is a refractive error that causes distant objects to appear blurry while close objects can be seen clearly. It occurs when the eyeball is too long or the cornea is too curved. Myopia can be managed with eyeglasses, contact lenses, or refractive surgery.",
+    url: "https://www.aao.org/eye-health/diseases/myopia-nearsightedness"
+  },
+  {
+    name: "Night Blindness",
+    description: "Night blindness, also known as nyctalopia, is a condition where an individual experiences difficulty seeing in low-light conditions or at night. It can be caused by various factors, including vitamin A deficiency, genetic conditions, or retinal diseases. Management may involve addressing the underlying cause and using strategies to enhance night vision.",
+    url: "https://www.healthline.com/health/night-blindness"
+  },
+  {
+    name: "Nystagmus",
+    description: "Nystagmus is an involuntary rhythmic movement of the eyes. It can be present from birth (congenital) or acquired due to underlying neurological conditions, medications, or other factors. Nystagmus can result in reduced vision and depth perception. Management aims to improve visual function and minimize associated symptoms.",
+    url: "https://www.aao.org/eye-health/diseases/nystagmus"
+  },
+  {
+    name: "Ocular Hypertension",
+    description: "Ocular hypertension refers to elevated intraocular pressure (IOP) within the eye that is higher than normal but not yet at a level to be diagnosed as glaucoma. It is a significant risk factor for developing glaucoma. Regular eye exams and monitoring are essential to detect any progression or changes in ocular hypertension.",
+    url: "https://www.aao.org/eye-health/diseases/ocular-hypertension"
+  },
+  {
+    name: "Ocular Melanoma",
+    description: "Ocular melanoma is a rare form of eye cancer that develops in the cells that produce pigment. It can affect different parts of the eye, including the choroid, iris, or ciliary body. Ocular melanoma may not cause symptoms in its early stages, but it can lead to vision changes or eye discomfort as it progresses. Treatment options include surgery, radiation therapy, and other interventions.",
+    url: "https://www.aao.org/eye-health/diseases/ocular-melanoma"
+  },
+  {
+    name: "Optic Neuritis",
+    description: "Optic neuritis is an inflammation of the optic nerve, which transmits visual information from the eye to the brain. It often causes sudden vision loss, pain, and changes in color perception. Optic neuritis can be a symptom of multiple sclerosis or occur independently. Treatment may involve corticosteroids to reduce inflammation and manage symptoms.",
+    url: "https://www.aao.org/eye-health/diseases/optic-neuritis"
+  },
+  {
+    name: "Photokeratitis (Snow Blindness)",
+    description: "Photokeratitis, commonly known as snow blindness, is a painful eye condition caused by overexposure to ultraviolet (UV) radiation from the sun or other sources, such as welding arcs. It leads to inflammation of the cornea, similar to a sunburn. Symptoms include redness, pain, light sensitivity, and blurred vision. Rest and avoiding bright light can help with recovery.",
+    url: "https://www.aao.org/eye-health/diseases/photokeratitis"
+  },
+  {
+    name: "Pinguecula and Pterygium",
+    description: "Pinguecula and pterygium are growths that appear on the white part of the eye (sclera). Pinguecula is a small, yellowish bump, while pterygium is a wedge-shaped growth that can extend onto the cornea. They are often caused by prolonged exposure to UV light and dry, dusty environments. While usually benign, severe cases may require surgical removal.",
+    url: "https://www.aao.org/eye-health/diseases/pinguecula-pterygium"
+  },
+  {
+    name: "Presbyopia",
+    description: "Presbyopia is an age-related condition that affects the eye's ability to focus on close objects. It usually becomes noticeable around the age of 40 and continues to progress. Individuals with presbyopia may find it challenging to read small print or perform tasks that require near vision without the use of reading glasses or corrective lenses.",
+    url: "https://www.aao.org/eye-health/diseases/presbyopia"
+  },
+  {
+    name: "Ptosis (Drooping Eyelid)",
+    description: "Ptosis is a condition characterized by the drooping of the upper eyelid, which can partially or completely cover the eye's pupil. It can be congenital (present from birth) or acquired due to aging, muscle weakness, or neurological conditions. Ptosis can affect vision and may require surgical correction if severe.",
+    url: "https://www.aao.org/eye-health/diseases/ptosis-drooping-eyelid"
+  },
+  {
+    name: "Retinal Detachment",
+    description: "Retinal detachment occurs when the retina becomes separated from the back of the eye's inner lining. This condition is considered a medical emergency as it can lead to permanent vision loss if not promptly treated. Symptoms of retinal detachment may include sudden flashes of light, the appearance of floaters, and the sensation of a curtain-like shadow moving across the field of vision. Surgical intervention is typically required to reattach the retina and restore proper vision.",
+    url: "https://www.aao.org/eye-health/diseases/what-is-retinal-detachment"
+  },
+  {
+    name: "Retinitis Pigmentosa",
+    description: "Retinitis pigmentosa is a group of inherited eye disorders that lead to degeneration of the retina's light-sensitive cells. It often begins with night blindness and progresses to tunnel vision or even blindness. There is no cure, but visual aids, mobility training, and research studies may help manage the condition.",
+    url: "https://www.aao.org/eye-health/diseases/retinitis-pigmentosa"
+  },
+  {
+    name: "Stargardt Disease",
+    description: "Stargardt disease is a genetic disorder that causes progressive central vision loss, typically beginning in childhood or adolescence. It is characterized by the accumulation of lipofuscin, a fatty substance, in the retina's light-sensitive cells. Currently, there is no cure, but research and clinical trials are ongoing to explore potential treatments.",
+    url: "https://www.aao.org/eye-health/diseases/stargardt-disease"
+  },
+  {
+    name: "Strabismus",
+    description: "Strabismus, commonly known as crossed eyes, occurs when the eyes do not align properly and point in different directions. This misalignment can lead to double vision and reduced depth perception. Strabismus may develop during childhood and can be corrected with eyeglasses, eye exercises, or surgery.",
+    url: "https://www.aao.org/eye-health/diseases/strabismus"
+  },
+  {
+    name: "Uveitis",
+    description: "Uveitis refers to inflammation of the uvea, the middle layer of the eye that includes the iris, ciliary body, and choroid. This condition can cause redness, pain, light sensitivity, and vision disturbances. Uveitis can result from various causes, including infections, autoimmune diseases, and trauma. Prompt diagnosis and treatment are essential to prevent complications that could lead to vision loss. Treatment may involve anti-inflammatory medications, immunosuppressive drugs, and addressing the underlying cause of inflammation.",
+    url: "https://www.aao.org/eye-health/diseases/what-is-uveitis"
+  }
+];
