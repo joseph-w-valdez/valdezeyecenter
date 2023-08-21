f81b4abe0c84b0cbaa070a9a53469cbc31186877
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-08-21 18:47:56 +0000

    changed the favicon and adjusted metadata

--- valdezeyecenter/src/app/layout.tsx
+++ valdezeyecenter/src/app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from '@/components/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'ValdezEyeCenter',
+  title: 'Valdez Eye Center',
   description: 'Your Clear Path to Eye Care Excellence!',
 }
 
