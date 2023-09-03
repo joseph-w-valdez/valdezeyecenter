e95e027d34cf09eeb4e44fe224fe8bd1e01da453
Joseph Valdez <Joseph.W.Valdez@GMail.com>
2023-09-03 03:35:13 +0000

    implemented materialUI to add an autofill that works by both typing and clicking suggestions on the spotlight page

--- valdezeyecenter/src/components/Spotlight.tsx
+++ valdezeyecenter/src/components/Spotlight.tsx
@@ -1,75 +1,103 @@
 'use client'
-import { useState } from "react";
-import { healthConditions } from "@/data/healthConditions";
-import Link from "next/link";
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { healthConditions } from '@/data/healthConditions';
+import Link from 'next/link';
 
 export default function Spotlight() {
-  const [randomIndex, setRandomIndex] = useState<number>(Math.floor(Math.random() * healthConditions.length));
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [selectedCondition, setSelectedCondition] = useState<string | null>(null);
+  const [randomIndex, setRandomIndex] = useState<number>(
+    Math.floor(Math.random() * healthConditions.length)
+  );
+  const [selectedCondition, setSelectedCondition] = useState<string | null>(
+    null
+  );
 
-  const handleSelectionChange = (conditionName: string | null) => {
-    setSelectedCondition(conditionName);
-    setSearchTerm(""); // Clear search input after selecting a condition
-  };
+  const handleSelectionChange = (searchInput:any) => {
+    if (searchInput.innerText) {
+      setSelectedCondition(searchInput.innerText)
+    }
+    else {
+      const matchedCondition = healthConditions.find(
+        (condition) => {
+          return condition.name.toLowerCase() === searchInput.value?.toLowerCase()
+        }
+      );
+      if (matchedCondition) {
+        setSelectedCondition(matchedCondition.name)
+      } else {
+        setSelectedCondition(null)
+      }
+    }
+
+};
 
   const loadNewCondition = () => {
     const newIndex = Math.floor(Math.random() * healthConditions.length);
     setRandomIndex(newIndex);
-    handleSelectionChange(null);
+    handleSelectionChange('');
     window.scrollTo(0, 0);
   };
 
-  const handleSearchSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    const matchedCondition = healthConditions.find(condition =>
-      condition.name.toLowerCase() === searchTerm.toLowerCase()
-    );
-    if (matchedCondition) {
-      handleSelectionChange(matchedCondition.name);
-    }
-  };
-
-  const matchedCondition = healthConditions.find(condition =>
-    condition.name.toLowerCase() === searchTerm.toLowerCase()
-  );
-
   return (
     <div className="flex flex-col gap-12 w-2/3">
-      <h1>{selectedCondition ? selectedCondition : healthConditions[randomIndex].name}</h1>
-      <form onSubmit={handleSearchSubmit} className="text-2xl">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for conditions"
-          className="border border-gray-300 rounded-lg p-2 mr-8 w-full max-w-[700px]"
-          list="condition-list"
-        />
-        <datalist id="condition-list">
-          {healthConditions.map(condition => (
-            <option key={condition.name} value={condition.name} />
-          ))}
-        </datalist>
-      </form>
-      {searchTerm && !matchedCondition && (
-        <p className="text-red-600 italic text-xl">
-          No matches found for &apos;{searchTerm}
-        </p>
-      )}
+      <h1>
+        {selectedCondition
+          ? selectedCondition
+          : healthConditions[randomIndex].name}
+      </h1>
+      <Autocomplete
+        value={
+          selectedCondition
+            ? healthConditions.find((condition) => condition.name === selectedCondition) || null
+            : null
+        }
+        onChange={(e) => handleSelectionChange(e.target)}
+        options={healthConditions}
+        getOptionLabel={(option) => option.name}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            name='condition search'
+            label="Search for conditions"
+            variant="outlined"
+            onChange={(e)=> handleSelectionChange(e.target)}
+          />
+        )}
+      />
       <p className="text-xl min-h-[200px]">
         {selectedCondition
-          ? healthConditions.find(condition => condition.name === selectedCondition)?.description || ""
+          ? healthConditions.find(
+            (condition) => condition.name === selectedCondition
+          )?.description || ''
           : healthConditions[randomIndex].description}
       </p>
-      {healthConditions[randomIndex].smokingCessation?.url && (
+      {selectedCondition === 'Smoking and Eye Health' && (
         <p className="text-red-600 text-lg">
-          If you&apos;re a smoker, consider quitting to reduce your risk.
-          Learn more about <Link href={healthConditions[randomIndex].smokingCessation?.url || '/not-found'} className="underline text-blue-600 hover:text-blue-400" target="_blank" rel="noopener noreferrer">smoking cessation</Link>.
+          If you&apos;re a smoker, consider quitting to reduce your risk. Learn
+          more about{' '}
+          <Link
+            href={
+              healthConditions[randomIndex].url || '/not-found'
+            }
+            className="underline text-blue-600 hover:text-blue-400"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            smoking cessation
+          </Link>
+          .
         </p>
       )}
       <div className="flex justify-start w-full gap-8 flex-wrap">
-        <a href={healthConditions[randomIndex].url} target="_blank" rel="noopener noreferrer" className="w-fit text-xl underline text-blue-700 hover:text-blue-400">Learn more here!</a>
+        <a
+          href={healthConditions[randomIndex].url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-fit text-xl underline text-blue-700 hover:text-blue-400"
+        >
+          Learn more here!
+        </a>
         <button
           className="border border-2 border-black rounded-lg w-[200px] text-xl py-1 font-bold bg-blue-700 text-white hover:text-black hover:bg-blue-400"
           onClick={loadNewCondition}
